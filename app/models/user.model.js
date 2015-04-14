0451842be1809d28e1b23360e7732591c4238d7c
Tams Sokari <tams.sokari@andela.co>
2015-04-14 16:53:45 +0100

    create the model for the table

---
+++ app/models/user.model.js
@@ -0,0 +1,33 @@
+var dbConfig = require('./../../config/db-config');
+
+var knex = require('knex')(dbConfig.db[process.env.NODE_ENV]);
+
+var bookshelf = require('bookshelf')(knex);
+
+knex.schema.hasTable('users')
+  .then( function (exists) {
+    // body...
+    if (!exists) {
+      bookshelf.knex.schema.createTable('users', function(table) {
+        table.increments('id').primary();
+        table.string('first_name', 30);
+        table.string('last_name', 30);
+        table.string('email', 30).unique();
+        table.string('username', 30).unique();
+        table.string('password', 30);
+        table.boolean('admin').defaultTo(false);
+        table.timestamp('joined_on');
+        table.timestamp('last_update');
+      })
+      .then(function() {
+        console.log('Table has been created');
+      });
+    }
+  });
+
+var userModel = bookshelf.Model.extend({
+  tableName: 'users',
+  hasTimestamps: ['joined_on', 'last_update']
+});
+
+module.exports = userModel;
