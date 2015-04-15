95ebc8c0800091bb72e87a2465beb02e0033f759
Tams Sokari <tams.sokari@andela.co>
2015-04-15 17:20:46 +0100

    successful test runs on model

--- tests/users-api/user.model.spec.js
+++ tests/users-api/user.model.spec.js
@@ -1,32 +1,137 @@
 var userModel = require('./../../app/models/user.model');
+var dbConfig = require('./../../config/db-config');
+var knex = require('knex')(dbConfig.db[process.env.NODE_ENV]);
 
-var user;
+var user, user1, user2, users;
 
 describe('User API unit tests: ', function () {
   // body...
-  beforeEach(function (done) {
-    // body...
-    user = new userModel({
-      username: 'freedom',
-      password: 'gobbledeedock'
-    });
-    done();
-  });
 
-  describe('Create a Table and Save a new user - ', function(){
+  describe('Save a new user - ', function(){
+    beforeEach(function (done) {
+      // body...
+      user = userModel.forge({
+        first_name: 'papandrea',
+        last_name: 'roberto',
+        email: 'r2p2@jk.com',
+        username: 'modeerf',
+        password: 'gobbledeedock'
+      });
+      done();
+    });
+
+
     it('should save without problems', function (done) {
       // body...          
-      console.log('i got here');
       user.save()
-        .then(function (model) {
+        .then(function (user) {
           // body...
-          expect(model.attributes)
+          expect(user.attributes)
             .toEqual(jasmine.objectContaining({
-              "username": "freedom",
+              "username": "modeerf",
               "password": "gobbledeedock"
             }));
           done();
         });
     });
+
+    afterEach(function (done) {
+      knex('users').where('username', 'modeerf').del()
+        .then(function () {
+          // body...
+          done();
+        });
+    });
   });
+
+  describe('Find a user from the user table - ', function () {
+    // body...
+    beforeEach(function (done) {
+      // body...
+      user = userModel.forge({
+        first_name: 'papandrea',
+        last_name: 'roberto',
+        email: 'r2p2@jk.com',
+        username: 'modeerf',
+        password: 'gobbledeedock'
+      });
+      user.save()
+        .then(function() {
+          done();
+        });
+    });
+
+    it('should return saved user', function(done) {
+      user.fetch()
+        .then(function(user) {
+          console.log(user.get('first_name'));
+          expect(user.attributes).toEqual(jasmine.objectContaining({
+            first_name: 'papandrea',
+            last_name: 'roberto',
+            email: 'r2p2@jk.com',
+            username: 'modeerf',
+            password: 'gobbledeedock'
+          }));
+          done();          
+        });
+    });
+
+    afterEach(function (done) {
+      knex('users').where('username', 'modeerf').del()
+        .then(function(){
+          done();
+        });
+    });
+
+  });
+
+  describe('Find a number of users: ', function() {
+    beforeEach(function(done) {
+      user1 = userModel.forge({
+        first_name: 'mohini',
+        last_name: 'ufeli',
+        email: 'mo@ufeli.ng',
+        username: 'mohini',
+        password: '1434'
+      });
+
+      user2 = userModel.forge({
+        first_name: 'susan',
+        last_name: 'hastings',
+        email: 'sue@andela.co',
+        username: 'sue',
+        password: 'wo1232'
+      })
+
+      user1.save()
+        .then(function(){
+        });
+
+      user2.save()
+        .then(function(){
+          done();
+        });
+
+    });
+
+    it('it should fetch all users - ', function(done) {
+      userModel.fetchAll({require: true})
+        .then(function(users) {
+          console.log(users.models);
+          expect(users).toBeDefined();
+          done();
+        });
+
+    });
+
+    afterEach(function (done) {
+      // body...
+      knex('users').where('admin', false).del()
+        .then(function () {
+          // body...
+          done();
+        });
+    });
+  });
+
 });
