315f29c80dacbe7a58676eb58cd2502ecd7b7930
Tams Sokari <tams.sokari@andela.co>
2015-04-16 14:52:32 +0100

    allow knex debug in the terminal

--- config/db-config.js
+++ config/db-config.js
@@ -4,31 +4,32 @@ module.exports = (function () {
   return {
             db: { 
                   development: {
-                    'client': 'pg',
-                    'connection': {
-                      'host': 'localhost',
-                      'user': 'tams',
-                      'database': 'users'
+                    client: 'pg',
+                    connection: {
+                      host: 'localhost',
+                      user: 'tams',
+                      database: 'users'
                     }
                   },
 
                   test: {
-                    'client': 'pg',
-                    'connection': {
-                      'host': 'localhost',
-                      'user': 'tams',
-                      'database': 'users-test'
-                    }
+                    client: 'pg',
+                    connection: {
+                      host: 'localhost',
+                      user: 'tams',
+                      database: 'users-test'
+                    },
+                    debug: true
                   },
 
                   production: {
-                    'client': 'pg',
-                    'connection': {
-                      'port': 5432,
-                      'host': 'ec2-54-163-239-102.compute-1.amazonaws.com',
-                      'user': 'qdvjqttcufavvh',
-                      'password': '-7JYkkBA35vvLQR4B9LB7Mzivs',
-                      'database': 'd4rul9ngpaiqnb'
+                    client: 'pg',
+                    connection: {
+                      port: 5432,
+                      host: 'ec2-54-163-239-102.compute-1.amazonaws.com',
+                      user: 'qdvjqttcufavvh',
+                      password: '-7JYkkBA35vvLQR4B9LB7Mzivs',
+                      database: 'd4rul9ngpaiqnb'
                     }
                   }
 
