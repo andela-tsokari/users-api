4c921eab8d3588aefc774b746f727c5bddef5dd1
Tams Sokari <tams.sokari@andela.co>
2015-04-16 14:54:05 +0100

    run tests per user for create, read, update and delete

--- tests/users-api/user.model.spec.js
+++ tests/users-api/user.model.spec.js
@@ -1,18 +1,18 @@
-var userModel = require('./../../app/models/user.model');
+var user = require('./../../app/models/user.model')[0],
+    users = require('./../../app/models/user.model')[1];
 var dbConfig = require('./../../config/db-config');
 var knex = require('knex')(dbConfig.db[process.env.NODE_ENV]);
 
-var user, user1, user2, users;
-
+var testUser, testUsers;
 describe('User API unit tests: ', function () {
   // body...
 
   describe('Save a new user - ', function(){
     beforeEach(function (done) {
       // body...
-      user = userModel.forge({
-        first_name: 'papandrea',
-        last_name: 'roberto',
+      testUser = user.forge({
+        first_name: 'port',
+        last_name: 'harcourt',
         email: 'r2p2@jk.com',
         username: 'modeerf',
         password: 'gobbledeedock'
@@ -23,10 +23,10 @@ describe('User API unit tests: ', function () {
 
     it('should save without problems', function (done) {
       // body...          
-      user.save()
-        .then(function (user) {
+      testUser.save()
+        .then(function (testUser) {
           // body...
-          expect(user.attributes)
+          expect(testUser.attributes)
             .toEqual(jasmine.objectContaining({
               "username": "modeerf",
               "password": "gobbledeedock"
@@ -42,32 +42,32 @@ describe('User API unit tests: ', function () {
           done();
         });
     });
+
   });
 
   describe('Find a user from the user table - ', function () {
     // body...
     beforeEach(function (done) {
       // body...
-      user = userModel.forge({
-        first_name: 'papandrea',
-        last_name: 'roberto',
+      testUser = user.forge({
+        first_name: 'port',
+        last_name: 'harcourt',
         email: 'r2p2@jk.com',
         username: 'modeerf',
         password: 'gobbledeedock'
       });
-      user.save()
+      testUser.save()
         .then(function() {
           done();
         });
     });
 
     it('should return saved user', function(done) {
-      user.fetch()
-        .then(function(user) {
-          console.log(user.get('first_name'));
-          expect(user.attributes).toEqual(jasmine.objectContaining({
-            first_name: 'papandrea',
-            last_name: 'roberto',
+      testUser.fetch()
+        .then(function(testUser) {
+          expect(testUser.attributes).toEqual(jasmine.objectContaining({
+            first_name: 'port',
+            last_name: 'harcourt',
             email: 'r2p2@jk.com',
             username: 'modeerf',
             password: 'gobbledeedock'
@@ -85,39 +85,113 @@ describe('User API unit tests: ', function () {
 
   });
 
-  describe('Find a number of users: ', function() {
-    beforeEach(function(done) {
-      user1 = userModel.forge({
-        first_name: 'mohini',
-        last_name: 'ufeli',
-        email: 'mo@ufeli.ng',
-        username: 'mohini',
-        password: '1434'
+  describe('find a user and update any details -  ', function() {
+    beforeEach(function (done) {
+      // body...
+      testUser = user.forge({
+        first_name: 'port',
+        last_name: 'harcourt',
+        email: 'r2p2@jk.com',
+        username: 'modeerf',
+        password: 'gobbledeedock'
       });
-
-      user2 = userModel.forge({
-        first_name: 'susan',
-        last_name: 'hastings',
-        email: 'sue@andela.co',
-        username: 'sue',
-        password: 'wo1232'
-      })
-
-      user1.save()
-        .then(function(){
+      testUser.save()
+        .then(function() {
+          done();
         });
+    });
 
-      user2.save()
-        .then(function(){
+    it('should update the user details: ', function(done) {
+      user
+        .where({
+          first_name: 'port'
+        })
+        .save({
+          first_name: 'Las'
+        }, 
+        {
+          method: 'update',
+          patch: true
+        })
+        .then(function(user) {
+          expect(user.toJSON()).toEqual(jasmine.objectContaining({first_name: 'Las'}));
           done();
         });
 
     });
 
+    afterEach(function (done) {
+      knex('users').where('username', 'modeerf').del()
+        .then(function(){
+          done();
+        });
+      });
+    
+  });
+
+  describe('find a user and delete the user - ', function() {
+    beforeEach(function (done) {
+      // body...
+      testUser = user.forge({
+        first_name: 'port',
+        last_name: 'harcourt',
+        email: 'r2p2@jk.com',
+        username: 'modeerf',
+        password: 'gobbledeedock'
+      });
+      testUser.save()
+        .then(function() {
+          done();
+        });
+    });
+
+    it('should delete the selected user: ', function(done) {
+      user
+      .where({email: 'r2p2@jk.com'})
+      .destroy()
+      .then(function (user) {
+        expect(user.toJSON()).toEqual({});
+        done();
+      })
+    });
+
+    afterEach(function (done) {
+      knex('users').where('username', 'modeerf').del()
+        .then(function(){
+          done();
+        });
+      });
+    
+  });
+
+  xdescribe('Find a number of users: ', function() {
+    beforeEach(function(done) {
+      testUsers = users.forge([{
+        first_name: 'port',
+        last_name: 'harcourt',
+        email: 'r2p2@jk.com',
+        username: 'modeerf',
+        password: 'gobbledeedock'
+      }, {
+        first_name: 'port',
+        last_name: 'harcourt',
+        email: 'r2p@jk.com',
+        username: 'freedom',
+        password: 'gobbledeedock'
+      }])
+        .invokeThen('save')
+          .then(function () {
+            // body...
+            done();
+          });
+
+    });
+
     it('it should fetch all users - ', function(done) {
-      userModel.fetchAll({require: true})
+      console.log(testUser);
+      testUsers.fetch()
         .then(function(users) {
-          console.log(users.models);
+          console.log(users)
           expect(users).toBeDefined();
           done();
         });
@@ -138,14 +212,14 @@ describe('User API unit tests: ', function () {
   xdescribe('template: ', function() {
     beforeEach(function (done) {
       // body...
-      user = userModel.forge({
-        first_name: 'papandrea',
-        last_name: 'roberto',
+      testUser = user.forge({
+        first_name: 'port',
+        last_name: 'harcourt',
         email: 'r2p2@jk.com',
         username: 'modeerf',
         password: 'gobbledeedock'
       });
-      user.save()
+      testUser.save()
         .then(function() {
           done();
         });
@@ -157,6 +231,7 @@ describe('User API unit tests: ', function () {
           done();
         });
       });
+
   });
 
 });
