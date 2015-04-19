e49bd13ed5a061c4d697f386882e056659eb8865
Tams Sokari <tams.sokari@andela.co>
2015-04-19 07:49:47 +0100

    - modify export method. - add hosting port. - add secret for authentication

--- config/db-config.js
+++ config/db-config.js
@@ -1,39 +1,40 @@
-module.exports = (function () {
-  'use strict';
-  // body...
-  return {
-            db: { 
-                  development: {
-                    client: 'pg',
-                    connection: {
-                      host: 'localhost',
-                      user: 'tams',
-                      database: 'users'
-                    }
-                  },
+module.exports = {
+                    db: { 
+                          development: {
+                            client: 'pg',
+                            connection: {
+                              host: 'localhost',
+                              user: 'tams',
+                              database: 'users'
+                            },
+                            debug: true
+                          },
 
-                  test: {
-                    client: 'pg',
-                    connection: {
-                      host: 'localhost',
-                      user: 'tams',
-                      database: 'users-test'
-                    },
-                    debug: true
-                  },
+                          test: {
+                            client: 'pg',
+                            connection: {
+                              host: 'localhost',
+                              user: 'tams',
+                              database: 'users-test'
+                            },
+                            debug: true
+                          },
 
-                  production: {
-                    client: 'pg',
-                    connection: {
-                      port: 5432,
-                      host: 'ec2-54-163-239-102.compute-1.amazonaws.com',
-                      user: 'qdvjqttcufavvh',
-                      password: '-7JYkkBA35vvLQR4B9LB7Mzivs',
-                      database: 'd4rul9ngpaiqnb'
-                    }
-                  }
+                          production: {
+                            client: 'pg',
+                            connection: {
+                              port: 5432,
+                              host: 'ec2-54-163-239-102.compute-1.amazonaws.com',
+                              user: 'qdvjqttcufavvh',
+                              password: '-7JYkkBA35vvLQR4B9LB7Mzivs',
+                              database: 'd4rul9ngpaiqnb'
+                            }
+                          }
 
-                },
+                        },
 
-          };
-})();
+                    port: process.env.PORT || 4000,
+
+                    secret: 'us5rs-1p9'
+
+                  };
