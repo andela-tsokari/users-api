24fbc70dbda6502aa842886a0349fce8973525f5
Tams Sokari <tams.sokari@andela.co>
2015-04-22 07:20:40 +0100

    require cors and restrict cors access to only the gateway

--- config/app-config.js
+++ config/app-config.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var routes = require('./../app/routes/users.route');
+var cors = require('cors');
+var corsConfig = {
+  origin: 'http://user-subs.herokuapp.com'
+};
 
 module.exports = (function() {
   'use strict';
@@ -9,6 +13,7 @@ module.exports = (function() {
   app
     .use(bodyParser.json())
     .use(bodyParser.urlencoded({extended: true}))
+    .use(cors(corsConfig))
     .use('/api/v1', router);
   
   routes(router);
