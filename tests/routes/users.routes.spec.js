0d79c8b164fd45e0c7354641ea87dc11edc3d388
Tams Sokari <tams.sokari@andela.co>
2015-04-22 07:22:28 +0100

    scaffold for e2e tests

--- tests/routes/users.routes.spec.js
+++ tests/routes/users.routes.spec.js
@@ -1,1 +1,4 @@
-var request = require("supertest");
+var request = require('supertest');
+var user = require('./../../app/models/user.model')[0],
+    users = require('./../../app/models/user.model')[1];
+var app = require('./../../config/app-config');
