c58f3b61bac54921490a0cd4fb471de6a338e956
Tams Sokari <tams.sokari@andela.co>
2015-04-23 00:14:56 +0100

    change log for when server is running

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var db = require('./config/db-config');
 var app = require('./config/app-config');
 
 app.listen(db.port, function() {
-  console.log('Users-Api listening on port: ' + db.port);
+  console.log('Users-API listening on Port: ' + db.port);
 });
