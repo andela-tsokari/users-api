fcfc68166e055122b1d3e05ae5184973158f17be
Tams Sokari <tams.sokari@andela.co>
2015-04-22 07:19:31 +0100

    require express-jwt for authentication verification on the update and delete requests, also require for private user profile.

--- app/routes/users.route.js
+++ app/routes/users.route.js
@@ -1,22 +1,28 @@
 var users = require('./../controllers/users.controllers');
+var restrict = require('./../../config/db-config').secret;
+var expressJWT = require('express-jwt');
 
 module.exports = function(router) {
   router
     .route('/signup')
-      .post(users.signUp);
+    .post(users.signUp);
 
   router
     .route('/login')
-      .post(users.login);
+    .post(users.login);
 
   router
     .route('/users')
-      .get(users.getAllUsers);
+    .get(users.getAllUsers);
+
+  router
+    .route('/:username')
+    .get(expressJWT({secret: restrict}), users.getOneUser)
+    .put(expressJWT({secret: restrict}), users.updateOneUser)
+    .delete(expressJWT({secret: restrict}), users.deleteUser);
 
   router
     .route('/user/:username')
-      .get(users.getOneUser)
-      .put(users.updateOneUser)
-      .delete(users.deleteUser);
+    .get(users.getOneUser);
 
 };
