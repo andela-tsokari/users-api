fe15223b56618cd63172471c2f556a5a30f13aea
Tams Sokari <tams.sokari@andela.co>
2015-04-23 18:40:02 +0100

    deleted test files

--- tests/routes/users.routes.spec.js
+++
@@ -1,4 +0,0 @@
-var request = require('supertest');
-var user = require('./../../app/models/user.model')[0],
-    users = require('./../../app/models/user.model')[1];
-var app = require('./../../config/app-config');
